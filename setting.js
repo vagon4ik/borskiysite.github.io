8369b1c5f6433f2961d32a695e85154c95c9e94a
vagon4ik <64847814+vagon4ik@users.noreply.github.com>
2020-08-29 20:23:44 +0300

    Create setting.js

---
+++ setting.js
@@ -0,0 +1,4 @@
+const MONITOR_SERVER_ADDERS   = '46.174.49.134',                // Адрес сервера
+      MONITOR_SERVER_PORT     = '27222',                    // Порт сервера. Диапозон от 20000 до 30000
+      MONITOR_SERVER_ENGINE   = 0,                          // Тип движка. 0 - Source, 1 - GoldSource.
+      MONITOR_BASE_URL        = 'https://kruzya.me/query/'; // Ссылка на обработчик. Подробнее: https://goo.gl/856nuZ
